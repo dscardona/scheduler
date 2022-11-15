ff9bf80ed182b5aaf2e04cc5eb13d42453e6cbff
Diana Cardona <dscardona@gmail.com>
2022-11-15 16:03:26 -0500

    add first migration with alembic

---
+++ alembic/versions/63c2cb30f2d3_create_schedulers.py
@@ -0,0 +1,24 @@
+"""create schedulers
+
+Revision ID: 63c2cb30f2d3
+Revises: 
+Create Date: 2022-11-15 16:02:16.411405
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '63c2cb30f2d3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
